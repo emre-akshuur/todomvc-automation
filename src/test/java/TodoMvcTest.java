9ec8be41cd61c1fd5b09f26e7e157f0fc3a6fdff
Liam <peynadol@gmail.com>
2026-05-27 13:56:27 +0100

    add tests

--- src/test/java/TodoMvcTest.java
+++ src/test/java/TodoMvcTest.java
@@ -1,14 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.*;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,13 +28,65 @@ public class TodoMvcTest {
     }
 
     @Test
+//    Consider the need for this test
     void testClickReact() throws Exception {
         driver.get("https://todomvc.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
         WebElement reactLink = driver.findElement(By.cssSelector("a[href*='react']"));
+
+//        Alternative way of locating - not sure which is more stable
+//        WebElement reactLink = driver.findElement(By.partialLinkText("React"));
+
         reactLink.click();
         takeScreenshot(driver, "todomvc_react.png");
+    }
+
+    @Test
+    void addValidTodo(){
+        driver.get("https://todomvc.com/");
+        WebElement reactLink = driver.findElement(By.partialLinkText("React"));
+        reactLink.click();
+
+        WebElement input = driver.findElement(By.id("todo-input"));
+        input.sendKeys("Buy milk", Keys.RETURN);
+
+        WebElement todoLabel = driver.findElement(By.cssSelector("[data-testid='todo-item-label'"));
+        String todoText = todoLabel.getText();
+        assertEquals("Buy milk", todoText);
+    }
+
+    @Test
+    void addMultipleValidTodos(){
+        driver.get("https://todomvc.com/");
+        WebElement reactLink = driver.findElement(By.partialLinkText("React"));
+        reactLink.click();
+
+        WebElement input = driver.findElement(By.id("todo-input"));
+        input.sendKeys("Buy milk", Keys.RETURN);
+        input.sendKeys("Wash up", Keys.RETURN);
+        input.sendKeys("Dry clothes", Keys.RETURN);
+
+
+        List<WebElement> todos = driver.findElements(By.cssSelector("[data-testid='todo-item-label'"));
+
+//      I've considered fragility in relation to order here. Current assuming insertion order is preserved.
+//      If this proves fragile - consider alternate assertion method that is order agnostic
+
+        String todo1 = todos.get(0).getText();
+        String todo2 = todos.get(1).getText();
+        String todo3 = todos.get(2).getText();
+
+        assertEquals("Buy milk", todo1);
+        assertEquals("Wash up", todo2);
+        assertEquals("Dry clothes", todo3);
+
+//        An inline version
+//        assertEquals("Buy milk", todos.get(0).getText());
+//        assertEquals("Wash up", todos.get(1).getText());
+//        assertEquals("Dry clothes", todos.get(2).getText());
+
     }
 
     @AfterAll

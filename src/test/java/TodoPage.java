150dcd8c9208b6e96a793af6f98d90ccca98cb1d
Emre <aksuur@hotmail.com>
2026-05-28 10:18:54 +0100

    Added test for no character

--- src/test/java/TodoPage.java
+++ src/test/java/TodoPage.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.*;
 
 public class TodoPage {
+    private WebDriver driver;
+    private By todoInput = By.id("todo-input");
+    private By todoItemLabel = By.cssSelector(("[data-testid='todo-item-label']"));
+    private By editInputLocator = By.cssSelector(("[data-testid='todo-item'] [data-testid='text-input']"));
+
 }
